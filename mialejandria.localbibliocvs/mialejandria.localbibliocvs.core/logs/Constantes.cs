59622c8792ea777335c598afffcccc478a6830c0
mikau86 <zeldapuente@hotmail.com>
2014-04-28 10:40:34 +0200

    Cambio de namespaces en 'logs'

---
+++ mialejandria.localbibliocvs/mialejandria.localbibliocvs.core/logs/Constantes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mialejandria.localbibliocvs.core.logs
+{
+    public class Constantes
+    {
+        public const string APP_NAME = "Nombre app";
+        public const string APP_VERSION = "0.1.2";
+
+        public static String DirectorioActual { get; set; }
+
+        public class TipoAccion
+        {
+            public const string Error = "Error";
+            public const string Info = "Info";
+        }
+        public class Prioridad
+        {
+            public const string Extrema = "Extrema";
+            public const string Grave = "Grave";
+            public const string Alta = "Alta";
+            public const string Normal = "Normal";
+            public const string Baja = "Baja";
+            public const string Nula = "Nula";
+        }
+
+        public static void CargarParametrosIniciales()
+        {
+            //cargar directorio actual
+            DirectorioActual = System.IO.Directory.GetCurrentDirectory();
+
+            mialejandria.localbibliocvs.core.logs.Logs.initLog();
+
+        }
+    }
+}
